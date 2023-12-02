eb7b2d45ace718ea3625c6be27375713321ffb44
Nikita Kravchuk <krabchuk@gmail.com>
2023-12-02 16:40:39 +0000

    ENH: Small refactoring

---
+++ agents/random.py
@@ -0,0 +1,14 @@
+from random import randint
+
+class RandomAgent:
+    def __init__(self, board_dim):
+        self.board_dim = board_dim
+
+    @staticmethod
+    def get_move(board):
+        available_cells = []
+        for i in range(board.board_dim):
+            for j in range(board.board_dim):
+                if board.board[i][j] == 0:
+                    available_cells.append((i,j))
+        return available_cells[randint(0, len(available_cells) - 1)]
